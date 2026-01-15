0d893889249fd50617fe910ed6cec5a9e9875dc3
Vinishyamala P <157477193+Vini123vini@users.noreply.github.com>
2026-01-15 19:28:38 +0530

    day2 task

--- studentsreport.sql
+++ studentsreport.sql
@@ -35,3 +35,43 @@ select max(Marks) as Highest_mark from student_Details;
 select AVG(Marks) as Average_mark from student_Details;
 
 select Name,Marks from student_Details where Marks=(select max(Marks) from student_Details);
+
+--Day 2
+use sql_practicing;
+create table sales(
+id int,
+product varchar(50),
+region varchar(50),
+salesperson varchar(50),
+quantity int,
+revenue int);
+
+INSERT INTO sales VALUES
+(1, 'Laptop', 'Chennai', 'Arun', 3, 180000),
+(2, 'Mobile', 'Coimbatore', 'Ravi', 5, 75000),
+(3, 'Tablet', 'Madurai', 'Meena', 2, 40000),
+(4, 'Laptop', 'Chennai', 'Divya', 1, 60000),
+(5, 'Mobile', 'Trichy', 'Karthi', 4, 60000),
+(6, 'Laptop', 'Coimbatore', 'Ravi', 2, 120000),
+(7, 'Tablet', 'Chennai', 'Arun', 3, 60000),
+(8, 'Mobile', 'Madurai', 'Meena', 6, 90000);
+
+select count(*)from sales;
+
+select sum(revenue) as Total_Revenue from sales;
+
+select avg(revenue) as Average_Revenue_Per_Sales from sales;
+
+select region,avg(revenue) as Average_sales_by_Region from sales group by region ;
+
+select product,avg(revenue) as Average_sales_by_product from sales group by product;
+
+select salesperson,count(*) as sales_count_by_person from sales group by salesperson;
+
+select region,max(revenue) as Highestsales_Region from sales group by region limit 1;
+
+select product,count(*) as selling_Product_Count from sales group by product;
+
+select salesperson from sales where quantity>2;
+
+select distinct product from sales where revenue > 100000;
