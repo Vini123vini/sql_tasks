839b70673b0c042b99208b9b0699fb0442a99c2e
Vinishyamala P <157477193+Vini123vini@users.noreply.github.com>
2026-01-18 19:53:26 +0530

    day3

--- studentsreport.sql
+++ studentsreport.sql
@@ -1,3 +1,5 @@
+--Day1--
+
 create database sql_practicing;
 use sql_practicing;
 
@@ -36,7 +38,7 @@ select AVG(Marks) as Average_mark from student_Details;
 
 select Name,Marks from student_Details where Marks=(select max(Marks) from student_Details);
 
---Day 2
+--Day 2--
 use sql_practicing;
 create table sales(
 id int,
@@ -75,3 +77,101 @@ select product,count(*) as selling_Product_Count from sales group by product;
 select salesperson from sales where quantity>2;
 
 select distinct product from sales where revenue > 100000;
+
+--Day3--
+
+use sql_practicing;
+
+CREATE TABLE clients (
+    client_id INT PRIMARY KEY,
+    client_name VARCHAR(50),
+    city VARCHAR(50)
+);
+CREATE TABLE items (
+    item_id INT PRIMARY KEY,
+    item_name VARCHAR(50),
+    price INT
+);
+
+CREATE TABLE delivery (
+    sale_id INT PRIMARY KEY,
+    client_id INT,
+    item_id INT,
+    quantity INT,
+    sale_date DATE,
+    total_price INT
+);
+INSERT INTO clients VALUES
+(1, 'Kumar', 'Chennai'),
+(2, 'Divya', 'Coimbatore'),
+(3, 'Suresh', 'Madurai');
+
+INSERT INTO items VALUES
+(201, 'TV', 45000),
+(202, 'Fridge', 30000),
+(203, 'Washing Machine', 25000);
+
+INSERT INTO delivery VALUES
+(501, 1, 201, 1, '2026-01-10', 45000),
+(502, 1, 203, 1, '2026-01-11', 25000),
+(503, 2, 202, 1, '2026-01-12', 30000),
+(504, 3, 201, 2, '2026-01-13', 90000);
+
+SELECT d.sale_id, c.client_name, d.sale_date, d.total_price
+FROM delivery d
+JOIN clients c ON d.client_id = c.client_id;
+
+select i.item_name,d.sale_date
+from items i
+join delivery d on i.item_id=d.item_id;
+
+select i.item_id,i.item_name,d.quantity,c.client_name
+from items i
+join delivery d on i.item_id = d.item_id
+join clients c on d.client_id = c.client_id;
+
+select c.client_name,
+count(d.sale_id)as Total_orders
+from clients c
+join delivery d on d.client_id = c.client_id
+group by c.client_name;
+
+select c.client_name, 
+  SUM(d.quantity * d.total_price) as Sales_Amount
+  from delivery d
+join clients  c on c.client_id=d.client_id
+join items i on i.item_id=d.item_id
+group by c.client_name;
+
+select i.item_name,
+sum(d.quantity)as Total_Quantity_sold
+from delivery d
+join items i on i.item_id=d.item_id
+group by i.item_name;
+
+select c.client_name,
+count(d.sale_id)as total_orders
+from delivery d
+join clients c on c.client_id=d.client_id
+group by c.client_name
+having count(d.sale_id)>1;
+
+select d.sale_id,i.item_name,c.client_name,d.sale_date,d.quantity
+from delivery d
+join clients c on c.client_id=d.client_id
+join items i on i.item_id=d.item_id
+where c.city ='chennai';
+
+select i.item_name,
+sum(d.quantity*d.total_price)as Total_Price
+from items i
+join delivery d on i.item_id=d.item_id
+group by i.item_name;
+
+select i.item_name,
+sum(d.quantity)as total_sold
+from delivery d
+join items i on d.item_id=i.item_id
+group by i.item_name
+order by total_sold  desc
+limit 1;
